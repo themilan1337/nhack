2141706db8d400fd3e3c09985300013444f6843e
Milan <76396495+themilan1337@users.noreply.github.com>
2025-09-13 19:47:04 +0500

    project init and added maps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxt/fonts', '@nuxt/image', 'nuxt-mapbox'],
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWlsYW4xMzM3IiwiYSI6ImNtZmliNGw1NjBqMHUya3F3N2ZlYmI4dGkifQ.1wSnZEu14y4snJroP8xOYA'
+  }
 })
